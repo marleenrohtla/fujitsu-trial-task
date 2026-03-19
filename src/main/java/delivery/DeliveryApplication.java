7de3b0cebcbaf5060d57be12f09042efa86b4b47
Marleen Rohtla <marleenrohtla@hotmail.com>
2026-03-19 20:06:24 +0200

    added spring boot

--- src/main/java/delivery/DeliveryApplication.java
+++
@@ -1,6 +0,0 @@
-package delivery;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class DeliveryApplication {
-    public static void main(String[] args) {
